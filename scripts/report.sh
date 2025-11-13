b286dca346d1c1d9ef6b115da514a0eb327236ea
Ziemniaczany Książe <mkulczyk@student.agh.edu.pl>
2025-11-13 14:22:56 +0100

    poprawki do skryptu

--- scripts/report.sh
+++ scripts/report.sh
@@ -1,15 +1,19 @@
-$DIR = "${1:-}"
+set -eu
 
-if [-z "$DIR"]; then
-    echo "Nie podano ścieżki katalogu. Użycie bash report.sh <KATALOG>";
-    exit 1;
+DIR="${1:-}"
+
+if [ -z "$DIR" ]; then
+    echo "Nie podano ścieżki katalogu. Użycie bash report.sh <KATALOG>"
+    exit 1
 fi
 
-if [ ! -d "$DIR"]; then
-    echo "Podany katalog nie istnieje";
-    exit 1;
+if [ ! -d "$DIR" ]; then
+    echo "Podany katalog nie istnieje"
+    exit 1
 fi 
     
-date "%Y-%m-%d %H-%M-%S"
+date "+%Y-%m-%d %H-%M-%S"
 
-eval "du -sh "$DIR"/*/ | sort -h | tail -5"
+printf "du -sh "$DIR"/* | sort -h | tail -5"
+
+exit 0
